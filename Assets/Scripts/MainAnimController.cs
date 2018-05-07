541de0af4cd5ec044fcba7b7ae5e04dfe682014d
Samuel Ortega <samuelortega@samuels-air-2.attlocal.net>
2018-05-07 15:20:22 -0700

    Update

--- Assets/Scripts/MainAnimController.cs
+++ Assets/Scripts/MainAnimController.cs
@@ -8,15 +8,30 @@ public class MainAnimController : MonoBehaviour {
 
 	public float speed = 5.0f;
 	public float rotationSpeed = 90.0f;
-
+	public GameObject target;
+	public float rotateSpeed = 5;
+	Vector3 offset;
 	private bool IsRunning, IsShooting, Dancing, IsCrouching, Jump, IsGathering, IsReloading, RiflePunch;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-		
+		offset = target.transform.position - transform.position;
+
 	}
-	
+		
+
+	void LateUpdate() {
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		target.transform.Rotate(0, horizontal, 0);
+
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		transform.position = target.transform.position - (rotation * offset);
+
+		transform.LookAt(target.transform);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		float translation = Input.GetAxis ("Vertical")*speed;
@@ -24,17 +39,20 @@ public class MainAnimController : MonoBehaviour {
 		translation *= Time.deltaTime;
 		rotation *= Time.deltaTime;
 
+		//animations
 		transform.Translate (0, 0, translation);
 		transform.Rotate (0, rotation, 0);
 
 		if(translation != 0){
 			anim.SetBool ("IsWalking", true);
+			speed = 5.0f;
 		} else{
 			anim.SetBool ("IsWalking", false);
 		}
 
 		if (Input.GetKey (KeyCode.LeftShift)) {
 			IsRunning = true;
+			speed = 10.0f;
 		} else {
 			IsRunning = false;
 		}
